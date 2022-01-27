169a1a2ad256b02a14fae04dea8cd20e0bc14a3f
MInhNguyen <MInhNguyen@gm.uit.edu.vn>
2022-01-27 15:24:48 +0700

    chore: init product enum

---
+++ src/constant/product.js
@@ -0,0 +1,54 @@
+export const LAPTOP_LINE_BY_BRANCHES = Object.freeze({
+  ASUS: ['Vivobook', 'Zenbook', 'TUF/ROG'],
+  MSI: ['GF Series', 'GT Series', 'TUF/ROG']
+})
+
+export const LAPTOP_BRANCHES = Object.freeze({
+  ASUS: 'ASUS'
+})
+
+export const KEYBOARD_BRANCHES = Object.freeze({
+  ASUS: 'ASUS'
+})
+
+export const MOUSE_BRANCHES = Object.freeze({
+  ASUS: 'ASUS'
+})
+
+export const CPU_BRANCHES = Object.freeze({
+  AMD: 'AMD',
+  INTEL: 'INTEL'
+})
+
+export const SSD_BRANCHES = Object.freeze({
+  ASUS: 'ASUS'
+})
+
+export const VGA_BRANCHES = Object.freeze({
+  ASUS: 'ASUS'
+})
+
+export const RAM_BRANCHES = Object.freeze({
+  ASUS: 'ASUS'
+})
+
+export const MONITOR_BRANCHES = Object.freeze({
+  ASUS: 'ASUS'
+})
+
+export const CASE_BRANCHES = Object.freeze({
+  ASUS: 'ASUS'
+})
+
+export const PRODUCT_TYPES = Object.freeze({
+  LAPTOP: 'LAPTOP',
+  KEYBOARD: 'KEYBOARD',
+  MOUSE: 'MOUSE',
+  CPU: 'CPU',
+  SSD: 'SSD',
+  VGA: 'VGA',
+  RAM: 'RAM',
+  PSU: 'PSU',
+  MONITOR: 'MONITOR',
+  CASE: 'CASE'
+})
