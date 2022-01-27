10da77c95204a309f283961d9da940e7112ced4a
minhpn15 <minhpn.nc@gmail.com>
2022-01-27 18:13:53 +0700

    update product enum

--- src/constant/product.js
+++ src/constant/product.js
@@ -1,54 +1,113 @@
-export const LAPTOP_LINE_BY_BRANCHES = Object.freeze({
-  ASUS: ['Vivobook', 'Zenbook', 'TUF/ROG'],
-  MSI: ['GF Series', 'GT Series', 'TUF/ROG']
-})
+// export const LAPTOP_LINE_BY_BRANCHES = Object.freeze({
+//   ASUS: ['Vivobook', 'Zenbook', 'TUF/ROG'],
+//   MSI: ['GF Series', 'GT Series', 'GL Series']
+// })
 
 export const LAPTOP_BRANCHES = Object.freeze({
-  ASUS: 'ASUS'
+  ASUS: ['Vivobook', 'Zenbook', 'TUF/ROG'],
+  ACER: ['Nitro', 'Predator', 'Aspire'],
+  MSI: ['GF Series', 'GT Series', 'GL Series'],
+  DELL: ['Inspiron', 'Vostro', 'Alieware'],
+  HP: ['Elitebook', 'Zbook', 'Envy/Spectre'],
+  APPLE: ['Macbook Pro', 'Macbook Air']
 })
 
 export const KEYBOARD_BRANCHES = Object.freeze({
-  ASUS: 'ASUS'
+  LOGITECH: 'LOGITECH',
+  RAZER: 'RAZER',
+  CORSAIR: 'CORSAIR',
+  AKKO: 'AKKO',
+  DAREU: 'DAREU',
+  NEWMEN: 'NEWMEN'
 })
 
 export const MOUSE_BRANCHES = Object.freeze({
-  ASUS: 'ASUS'
+  LOGITECH: 'LOGITECH',
+  RAZER: 'RAZER',
+  CORSAIR: 'CORSAIR',
+  AKKO: 'AKKO',
+  DAREU: 'DAREU',
+  NEWMEN: 'NEWMEN'
+})
+
+export const HEADPHONE_BRANCHES = Object.freeze({
+  LOGITECH: 'LOGITECH',
+  RAZER: 'RAZER',
+  CORSAIR: 'CORSAIR',
+  JBL: 'JBL',
+  DAREU: 'DAREU',
+  HYPERX: 'HYPERX'
+})
+
+export const MAINBOARD_BRANCHES = Object.freeze({
+  AMD: ['A320', 'B450', 'X470'],
+  INTEL: ['H610', 'B660', 'Z690']
 })
 
 export const CPU_BRANCHES = Object.freeze({
-  AMD: 'AMD',
-  INTEL: 'INTEL'
+  AMD: ['AMD Gen 3', 'AMD Gen 5', 'AMD Threadripper Gen 3'],
+  INTEL: ['INTEL Gen 10', 'INTEL Gen 11', 'INTEL Gen 12']
 })
 
 export const SSD_BRANCHES = Object.freeze({
-  ASUS: 'ASUS'
+  SAMSUNG: 'SAMSUNG',
+  WESTERN: ['BLUE', 'RED', 'PURPLE'],
+  KINGSTON: 'KINGSTON',
+  GIGABYTE: 'GIGABYTE',
+  PNY: 'PNY'
 })
 
 export const VGA_BRANCHES = Object.freeze({
-  ASUS: 'ASUS'
+  ASUS: ['TUF Series', 'ROGSTRIX Series'],
+  GIGABYTE: ['Gigabye GeForce', 'Gigabyte Aorus'],
+  MSI: ['MSI Ventus', 'MSI Gaming Z TRIO']
 })
 
 export const RAM_BRANCHES = Object.freeze({
-  ASUS: 'ASUS'
+  SAMSUNG: 'SAMSUNG',
+  CORSAIR: 'CORSAIR',
+  KINGSTON: 'KINGSTON',
+  GIGABYTE: 'GIGABYTE',
+  TEAMGROUP: 'TEAMGROUP'
 })
 
 export const MONITOR_BRANCHES = Object.freeze({
-  ASUS: 'ASUS'
+  ASUS: 'ASUS',
+  ACER: 'ACER',
+  DELL: 'DELL',
+  LG: 'LG',
+  VIEWSONIC: 'VIEWSONIC',
+  HKC: 'HKC',
+  AOC: 'AOC'
 })
 
 export const CASE_BRANCHES = Object.freeze({
-  ASUS: 'ASUS'
+  COOLERMASTER: 'COOLERMASTER',
+  XIGMATEK:'XIGMATEK',
+  NZXT: 'NZXT',
+  DEEPCOOL: 'DEEPCOOL',
+  CORSAIR: 'CORSAIR'
+})
+
+export const PSU_BRANCHES = Object.freeze({
+  COOLERMASTER: 'COOLERMASTER',
+  GIGABYE:'GIGABYE',
+  ASUS: 'ASUS',
+  XIGMATEK: 'XIGMATEK',
+  CORSAIR: 'CORSAIR'
 })
 
 export const PRODUCT_TYPES = Object.freeze({
   LAPTOP: 'LAPTOP',
   KEYBOARD: 'KEYBOARD',
   MOUSE: 'MOUSE',
+  HEADPHONE: 'HEADPHONE',
+  MAINBOARD: 'MAINBOARD',
   CPU: 'CPU',
   SSD: 'SSD',
   VGA: 'VGA',
   RAM: 'RAM',
-  PSU: 'PSU',
   MONITOR: 'MONITOR',
-  CASE: 'CASE'
+  CASE: 'CASE',
+  PSU: 'PSU'
 })
